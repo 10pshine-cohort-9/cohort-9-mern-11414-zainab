c8a70177f28c665aa5b1ec27733b1fcba0aa876d
zainabusman22 <zainabusman322@gmail.com>
2026-08-20 17:51:36 +0500

    Update Note.ts

--- server/models/Note.ts
+++ server/models/Note.ts
@@ -1,6 +1,6 @@
-import { DataTypes, Model, Optional } from "sequelize"
-import { sequelize } from "../config/db"
-import { User } from "./User"
+import { DataTypes, Model, Optional } from "sequelize" // Tools needed to create Note database model
+import { sequelize } from "../config/db" //db connection
+import { User } from "./User" //each Note belongs to a User
 
 interface NoteAttributes {
     id: number
@@ -11,9 +11,10 @@ interface NoteAttributes {
     updatedAt?: Date
 }
 
-type NoteCreationAttributes = Optional<NoteAttributes, "id">
+type NoteCreationAttributes = Optional<NoteAttributes, "id"> // ID is optional because the db generates it
 
 export class Note extends Model<NoteAttributes, NoteCreationAttributes> implements NoteAttributes {
+    // Creates the Sequelize model representing a Note
     declare id: number
     declare title: string
     declare content: string
@@ -23,6 +24,7 @@ export class Note extends Model<NoteAttributes, NoteCreationAttributes> implemen
 }
 
 Note.init(
+    //how the Note should be stored in the database
     {
         id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
         title: { type: DataTypes.STRING, allowNull: false, defaultValue: "Untitled note" },
@@ -36,5 +38,5 @@ Note.init(
     }
 )
 
-User.hasMany(Note, { foreignKey: "userId", onDelete: "CASCADE" })
-Note.belongsTo(User, { foreignKey: "userId" })
+User.hasMany(Note, { foreignKey: "userId", onDelete: "CASCADE" }) //one User, many Notes
+Note.belongsTo(User, { foreignKey: "userId" }) //each Note belongs to 1 User
